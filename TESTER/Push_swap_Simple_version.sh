b192fe81ccc7d53e6f857ce665cd19a2c62cf577
gkomba <kombagildo@gmail.com>
2024-08-29 16:49:48 +0100

    UPDATE

--- TESTER/Push_swap_Simple_version.sh
+++ TESTER/Push_swap_Simple_version.sh
@@ -134,6 +134,135 @@ OPTRS=$($EXEC "$INPUT" 2>&1 | wc -l)
 		echo -e "\033[0;31mKO!\033[0m"
 		echo "Failed at \"$INPUT\""
 	fi
+
+echo "Push_swap - Another Simple version | PART 1"
+
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC $INPUT | $CHECK $INPUT)
+OPTRS=$($EXEC $INPUT | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at ${INPUT}"
+	fi
+
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC $INPUT | $CHECK $INPUT)
+OPTRS=$($EXEC $INPUT | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at ${INPUT}"
+	fi
+
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC $INPUT | $CHECK $INPUT)
+OPTRS=$($EXEC $INPUT | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at ${INPUT}"
+	fi
+
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC $INPUT | $CHECK $INPUT)
+OPTRS=$($EXEC $INPUT | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at ${INPUT}"
+	fi
+
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC $INPUT | $CHECK $INPUT)
+OPTRS=$($EXEC $INPUT | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at ${INPUT}"
+	fi
+
+echo ""
+echo "Push_swap - Another Simple version | PART 2"
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC "$INPUT" 2>&1 | $CHECK "$INPUT" 2>&1)
+OPTRS=$($EXEC "$INPUT" 2>&1 | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	elif [ "${RES}" = "Error" ]; then
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	fi
+
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC "$INPUT" 2>&1 | $CHECK $INPUT 2>&1)
+OPTRS=$($EXEC "$INPUT" 2>&1 | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	elif [ "${RES}" = "Error" ]; then
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	fi
+
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC "$INPUT" 2>&1 | $CHECK $INPUT 2>&1)
+OPTRS=$($EXEC "$INPUT" 2>&1 | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	elif [ "${RES}" = "Error" ]; then
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	fi
+
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC "$INPUT" 2>&1 | $CHECK $INPUT 2>&1)
+OPTRS=$($EXEC "$INPUT" 2>&1 | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	elif [ "${RES}" = "Error" ]; then
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	fi
+
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC "$INPUT" 2>&1 | $CHECK $INPUT 2>&1)
+OPTRS=$($EXEC "$INPUT" 2>&1 | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	elif [ "${RES}" = "Error" ]; then
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	fi
 }
 
 Push_swap_Simple_version
--- TESTER/save.sh
+++ TESTER/save.sh
@@ -1,28 +1,139 @@
-INPUT="1 2 3"
-    if $VAL $EXEC $INPUT 2>&1 | grep -q "${LEAK_CHECK}"; then
-    echo -e "\033[0;32mOK!\033[0m"
+#!/bin/bash
+
+CHECK="./checker_linux"
+EXEC="../push_swap"
+TIME="sleep 0.2"
+
+
+Push_swap_Simple_version(){
+echo "Push_swap - Another Simple version | PART 1"
+
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC $INPUT | $CHECK $INPUT)
+OPTRS=$($EXEC $INPUT | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
 	else
 		echo -e "\033[0;31mKO!\033[0m"
 		echo "Failed at ${INPUT}"
 	fi
 
-for INPUT in "42" "2 3" "1 2 3 4 5 6 7 8 9" "2 3 4 5 6 7" "0 1 2 3 4" "100 200 300 400 500 600 700 800"
-    do
-		$TIME
-		if $VAL $EXEC "$INPUT" 2>&1 | grep -q "${LEAK_CHECK}"; then
-            echo -e "\033[0;32mOK!\033[0m"
-        else
-            echo -e "\033[0;31mKO!\033[0m"
-			echo "Failed at \"$INPUT\""
-		fi
-done
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC $INPUT | $CHECK $INPUT)
+OPTRS=$($EXEC $INPUT | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at ${INPUT}"
+	fi
 
-for INPUT in "42" "2 3" "1 2 3 4 5 6 7 8 9" "2 3 4 5 6 7" "0 1 2 3 4" "100 200 300 400 500 600 700 800"
-    do
-		$TIME
-		if $VAL $EXEC $INPUT 2>&1 | grep -q "${LEAK_CHECK}"; then           echo -e "\033[0;32mOK!\033[0m"
-        else
-            echo -e "\033[0;31mKO!\033[0m"
-			echo "Failed at ${INPUT}"
-		fi
-done
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC $INPUT | $CHECK $INPUT)
+OPTRS=$($EXEC $INPUT | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at ${INPUT}"
+	fi
+
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC $INPUT | $CHECK $INPUT)
+OPTRS=$($EXEC $INPUT | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at ${INPUT}"
+	fi
+
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC $INPUT | $CHECK $INPUT)
+OPTRS=$($EXEC $INPUT | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at ${INPUT}"
+	fi
+
+echo ""
+echo "Push_swap - Another Simple version | PART 2"
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC "$INPUT" 2>&1 | $CHECK "$INPUT" 2>&1)
+OPTRS=$($EXEC "$INPUT" 2>&1 | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	elif [ "${RES}" = "Error" ]; then
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	fi
+
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC "$INPUT" 2>&1 | $CHECK $INPUT 2>&1)
+OPTRS=$($EXEC "$INPUT" 2>&1 | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	elif [ "${RES}" = "Error" ]; then
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	fi
+
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC "$INPUT" 2>&1 | $CHECK $INPUT 2>&1)
+OPTRS=$($EXEC "$INPUT" 2>&1 | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	elif [ "${RES}" = "Error" ]; then
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	fi
+
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC "$INPUT" 2>&1 | $CHECK $INPUT 2>&1)
+OPTRS=$($EXEC "$INPUT" 2>&1 | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	elif [ "${RES}" = "Error" ]; then
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	fi
+
+$TIME
+INPUT=$(shuf -i 1-1000 -n 5)
+RES=$($EXEC "$INPUT" 2>&1 | $CHECK $INPUT 2>&1)
+OPTRS=$($EXEC "$INPUT" 2>&1 | wc -l)
+	if [[ "${RES}" = "OK" && "${OPTRS}" -le 12 ]]; then
+		echo -e "\033[0;32mOK!\033[0m"
+	elif [ "${RES}" = "Error" ]; then
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	else
+		echo -e "\033[0;31mKO!\033[0m"
+		echo "Failed at \"$INPUT\""
+	fi
+}
+
+Push_swap_Simple_version
